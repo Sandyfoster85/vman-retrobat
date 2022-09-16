e82a5b2623f0f492fd5442da0a3f465fa22297ad
virtual-man007 <centronic_solutions@hotmail.com>
2022-09-16 19:39:45 -0300

    # 2022-09-16 master gamelist/collections update

--- teknoparrot_retrobat/create_mklink.bat
+++ teknoparrot_retrobat/create_mklink.bat
@@ -8,6 +8,7 @@ mklink /J C:\FNFSC V:\RetroBat\roms\teknoparrot\FNFSC.parrot
 mklink /J C:\SCJC V:\RetroBat\roms\teknoparrot\SnoCross.parrot 
 mklink /J C:\FNFSB V:\RetroBat\roms\teknoparrot\FNFSB.parrot
 mklink /J C:\FNFSB2 V:\RetroBat\roms\teknoparrot\FNFSB2.parrot
+mklink /J "C:\Sega Racing Classic" V:\RetroBat\roms\teknoparrot\SRC.parrot
 
 echo mklink created
 pause
